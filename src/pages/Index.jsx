bc2c815b06e0fa98b2760751bb6c5e921936bf24
Christian Alexander <christian.alexanderato@gmail.com>
2024-04-03 14:41:01 -0700

    updates to styling - beginning code cleanup

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -5,11 +5,14 @@
   const allKits = useLoaderData()
    return (
     <div className="container">
-     <Link to="/create">
-     <button>Add +</button>
-     </Link>
      <div className="list">
       <div className="gallery">
+        <div className="add-button">
+          <Link to="/create">
+     <button>Add +</button>
+     </Link>
+     </div>
+     
         {allKits.map((kit, i) => <Kit kit={kit} key={i}/>)}
       </div>
     </div>
