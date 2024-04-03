12a62555c673cc160a336a0b0c46d639aebbda2a
Christian Alexander <christian.alexanderato@gmail.com>
2024-04-02 23:12:29 -0700

    new updates

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -9,8 +9,28 @@
       <div className="container">
      <div className="card">
         <img src={kit.box_art} alt={kit.box_art} />
+      <div className="id">
         <h1>{kit.name}</h1>
-        <h2>Bandai Kit #{kit.model_id}</h2>
+        <h2>ID #{kit.model_id}</h2>
+      </div>
+      <h3>{kit.series}</h3>
+        
+        <div className="lists">
+          <div>
+            <h2>prices</h2>
+            <ul>
+              <li>${kit.price_us}</li>
+              <li>&yen;{kit.price_jp} </li>
+            </ul>
+          </div>
+          <div>
+            <h2>Specs</h2>
+            <ul>
+              <li>{kit.grade}</li>
+              <li>{kit.scale}</li>
+            </ul>
+          </div>
+        </div>
         <h2>{kit.release_date}</h2>
      </div>
      <div className="buttons">
