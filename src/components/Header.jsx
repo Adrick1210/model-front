3f1650b10a8a3c12b0b9e2b5c9963f4bc2f8f5d4
Christian Alexander <christian.alexanderato@gmail.com>
2024-04-02 22:20:38 -0700

    header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ function Header() {
   return (
     <div className="header">
       <h1>Model Meister</h1>
+      <img src="\public\gundam_exia_by_etiger1995_d91qpz2-fullview.png" alt="icon" />
     </div>
   )
 }
