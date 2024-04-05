0b21ef36bd2509e29a51f0e51e691490a021f35b
Christian Alexander <christian.alexanderato@gmail.com>
2024-04-04 21:34:56 -0700

    added new logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,6 +8,9 @@ function Footer() {
         <Link to="https://github.com/Adrick1210">
         <img src="https://pngimg.com/d/github_PNG83.png" alt="git hub" />
         </Link>
+        <Link to="https://www.facebook.com/Alexander.Christian.25">
+        <img src="https://img.freepik.com/premium-psd/3d-square-with-facebook-logo_901970-2.jpg" alt="facebook" />
+        </Link>
         <Link to="https://www.linkedin.com/in/christianalexander23/">
         <img src="https://gmlconsult.com/wp-content/uploads/2021/10/Illustration-of-Linkedin-icon-on-transparent-background-PNG.png" alt="LinkedIn" />
         </Link>
