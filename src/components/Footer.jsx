902efae04263d92bc5d1083f9047c23bb9cb404e
Christian Alexander <christian.alexanderato@gmail.com>
2024-04-04 21:41:54 -0700

    new icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,10 @@ function Footer() {
         <h5>Adrick-Development &copy; 2024</h5>
         <div className="links">
         <Link to="https://github.com/Adrick1210">
-        <img src="https://pngimg.com/d/github_PNG83.png" alt="git hub" />
+        <img src="https://seeklogo.com/images/G/github-colored-logo-FDDF6EB1F0-seeklogo.com.png" alt="git hub" />
         </Link>
         <Link to="https://www.facebook.com/Alexander.Christian.25">
-        <img src="https://img.freepik.com/premium-psd/3d-square-with-facebook-logo_901970-2.jpg" alt="facebook" />
+        <img src="https://www.edigitalagency.com.au/wp-content/uploads/facebook-logo-png-tiktok-version.png" alt="facebook" />
         </Link>
         <Link to="https://www.linkedin.com/in/christianalexander23/">
         <img src="https://gmlconsult.com/wp-content/uploads/2021/10/Illustration-of-Linkedin-icon-on-transparent-background-PNG.png" alt="LinkedIn" />
