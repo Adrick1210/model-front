344848783bc29c334e33ddec0e3f98e89567bb6c
Christian Alexander <christian.alexanderato@gmail.com>
2024-04-09 01:45:09 -0700

    form update

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import './App.css'
 import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import Nav from './components/Nav'
 
 function App() {
 
   return (
     <div className='App'>
       <Header />
-      <Nav />
       <Outlet />
       <Footer />
     </div>
