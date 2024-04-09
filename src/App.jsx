7be4a3118557f90d936a0ec1713542b9b3edba30
Christian Alexander <christian.alexanderato@gmail.com>
2024-04-09 01:38:52 -0700

    form needs to be fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Nav from './components/Nav'
 
 function App() {
 
   return (
     <div className='App'>
       <Header />
+      <Nav />
       <Outlet />
       <Footer />
     </div>
